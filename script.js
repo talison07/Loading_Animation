b44aea164a9833615c835ad56354dbebfdcd66ce
Talison Antunes <talisonantunes.dev@gmail.com>
2022-08-29 18:25:14 -0300

    CSS update

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 let button = document.getElementsByClassName("button");
 let loadingText = document.getElementsByClassName("loading_text");
+let completeText = document.getElementsByClassName("complete_text");
+let complete = document.getElementsByClassName("complete");
 
 function loading() {
     button[0].classList.remove("hover");
     button[0].classList.add("loading");
-    loadingText[0].classList.add("loading_text_appear");
-    
+    loadingText[0].classList.add("appear");
+
 
 
     setTimeout(() => {
-        loadingText[0].textContent = "Complete"
+        loadingText[0].classList.remove("appear");
+        button[0].style.opacity = 0;
+
+        setTimeout(() => { 
+            complete[0].classList.add("appear") 
+            completeText[0].classList.add("appear")
+        }, 700);
+
+
     }, 4100)
 
 };
